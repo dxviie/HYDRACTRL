3ddbb6d1f4d4b105fac9bd5c2d87b8aa42afef62
david <david@d17e.dev>
2025-05-04 22:10:28 +0200

    cleanup

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-// Export all components
-export { Component } from './Component.jsx';
-export { MonitorPanel } from './MonitorPanel.jsx';
-export { DraggableWrapper } from './DraggableWrapper.jsx';
